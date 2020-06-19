3f2cf7a3b95e19ce2f2588ae4861990094ac0481
Brian Lego <legobw@gmail.com>
2020-06-19 14:12:56 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,10 @@
 def mario
-  
   phrase = "It's-a me, Mario!"
   puts phrase
 end
+
 $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def toadstool
-  puts status
+  puts $status
 end
