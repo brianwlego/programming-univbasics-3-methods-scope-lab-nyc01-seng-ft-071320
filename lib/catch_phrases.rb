f6d65650e8e5f615e39d448ef2f2da5175bb299e
Brian Lego <legobw@gmail.com>
2020-06-19 14:16:09 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,10 +4,11 @@ def mario
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts status
+  phrase2 = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase2
 end
 
 def link 
+  phrase3 = 
   puts "It's Dangerous To Go Alone! Take This."
 end
